d8b1abf8d6a60927248f2e597849d2ceff7b98ed
Martin Halder <halderm@arkadia-labs.io>
2025-10-04 12:57:23 +0200

    feat: add fzf-lua plugin
    Add fuzzy finder for files, buffers, and grep

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,42 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "echasnovski/mini.icons" },
+  cmd = "FzfLua",
+  keys = {
+    { "<leader><leader>", "<cmd>FzfLua files<cr>", desc = "files" },
+    { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "grep" },
+    {
+      "<leader>fn",
+      function()
+        require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "neovim config",
+    },
+    {
+      "<leader>fc",
+      function()
+        require("fzf-lua").files({ cwd = "~/.config" })
+      end,
+      desc = "config files",
+    },
+    {
+      "<leader>fp",
+      function()
+        require("fzf-lua").files({ cwd = "~/.local/share/nvim/lazy" })
+      end,
+      desc = "plugins",
+    },
+    { "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "help" },
+    { "<leader>fk", "<cmd>FzfLua keymaps<cr>", desc = "keymaps" },
+    { "<leader>fm", "<cmd>FzfLua marks<cr>", desc = "marks" },
+    { "<leader>fz", "<cmd>FzfLua builtin<cr>", desc = "fzf builtin" },
+    { "<leader>fw", "<cmd>FzfLua grep_cword<cr>", desc = "word" },
+    { "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", desc = "WORD" },
+    { "<leader>fd", "<cmd>FzfLua diagnostics_document<cr>", desc = "diagnostics" },
+    { "<leader>fr", "<cmd>FzfLua resume<cr>", desc = "resume" },
+    { "<leader>fo", "<cmd>FzfLua oldfiles<cr>", desc = "old files" },
+    { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "buffers" },
+    { "<leader>/", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "grep buffer" },
+  },
+  opts = {},
+}
---
+++ plugin-docs/fzf-lua.md
@@ -0,0 +1,52 @@
+# fzf-lua
+
+Fuzzy finder for files, grep, buffers, and more.
+
+## Usage
+
+All commands are prefixed with `<leader>f`:
+
+### File Finding
+
+- `<leader><leader>` - Find files in current directory
+- `<leader>fn` - Find files in Neovim config
+- `<leader>fc` - Find files in ~/.config
+- `<leader>fp` - Find files in plugins directory
+- `<leader>fo` - Old/recent files
+
+### Search
+
+- `<leader>fg` - Live grep (search in files)
+- `<leader>fw` - Grep word under cursor
+- `<leader>fW` - Grep WORD under cursor
+- `<leader>/` - Grep in current buffer
+
+### Navigation
+
+- `<leader>fb` - Switch buffers
+- `<leader>fm` - Jump to marks
+- `<leader>fd` - Browse diagnostics
+
+### Help
+
+- `<leader>fh` - Search help tags
+- `<leader>fk` - Search keymaps
+- `<leader>fz` - Browse all fzf builtin pickers
+
+### Other
+
+- `<leader>fr` - Resume last picker
+
+## Inside FZF
+
+- `Ctrl-n/p` or `↓/↑` - Navigate items
+- `Enter` - Select item
+- `Ctrl-c` or `Esc` - Close
+- `Tab` - Mark multiple items
+- `Ctrl-/` - Toggle preview
+
+## Configuration
+
+Located in: `lua/plugins/fzf-lua.lua`
+
+Uses mini.icons for file icons.
